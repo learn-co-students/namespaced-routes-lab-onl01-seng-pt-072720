161bd811e3eb757a166e2d442aff34d7fe87d04a
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-04 18:15:27 -0500

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,11 @@
+class Admin::PreferencesController < ApplicationController
+    # When you create a new folder under /controllers, Rails will automatically pick that up as a module and expect you to namespace the controller accordingly.
+      def index
+        
+      end
+end
+    
+    # Top-tip: The views folder for a controller module (in this case /admin) 
+    # expects a subfolder structure that matches the names of the controllers 
+    # (in this case /admin/stats).
+    
